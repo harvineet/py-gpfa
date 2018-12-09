a7f1b9cda2fa1c83c19e08405ea45d14ee654a0b
harvineetsingh <harvineet1992@gmail.com>
2018-12-09 17:49:52 -0500

    Adding cross-validation. Testing remaining

--- extract_traj.py
+++ extract_traj.py
@@ -1,12 +1,13 @@
 # Extracting trajectory from given data
 
 import os
-from core_gpfa.gpfa_engine import gpfa_engine # CHECK if works
+from core_gpfa.gpfa_engine import gpfa_engine
+# import copy # CHECK if required
 
 def extract_traj(output_dir, data, method='gpfa', x_dim=3):
     
     bin_width = 20 # in msec # NOT REQUIRED
-    num_folds = 0
+    num_folds = 1 # number of splits, keep 1 for using all train data
     
     # Create results directory if not exists
     if not os.path.exists(output_dir):
@@ -16,25 +17,51 @@ def extract_traj(output_dir, data, method='gpfa', x_dim=3):
     # NOT REQUIRED, check input data format
 
     # Divide data into cross-validation train and test folds
-    # TODO, all train data
-    seq_train = data # TODO, change to numpy.array
-    seq_test = [] # TODO, change to numpy.array
+    # TODO, crossvalidation folds
+    N = len(data)
+    f_div = np.floor(np.linspace(0, N, num_folds+1))
 
-    # Remove inactive units based on training set
-    # TODO
+    for cvf in range(num_folds):
+        if num_folds==1:
+            print("Training on all data")    
+            # seq_train = data # TODO, change to numpy.array
+            # seq_test = [] # TODO, change to numpy.array
+        else:
+            print("Cross-validation fold %d of %d" % (cvf, num_folds))
+        
+        test_mask = np.zeros(N, dtype=bool)
+        if num_folds > 1:
+            test_mask[np.arange(f_div[cvf],f_div[cvf+1], dtype=int)] = True
+        train_mask = ~test_mask
 
-    # Check if training data covariance is full rank
-    # TODO
+        if num_folds == 1:
+            # Keep original order if using all data as training set
+            tr = np.arange(0, N)
+        else:
+            tr = np.random.permutation(N)
 
-    # If doing cross-validation, don't use private noise variance floor
-    # TODO, set minVarFrac and pass to gpfa_engine
+        train_trial_idx = tr[train_mask]
+        test_trial_idx  = tr[test_mask]
+        seq_train = [data[trial_num] for trial_num in train_trial_idx] # CHECK if copy.deepcopy() required
+        seq_test = [data[trial_num] for trial_num in test_trial_idx]
+        print(len(seq_train), len(seq_test))
+        # Remove inactive units based on training set
+        # TODO
 
-    # Name of results file
-    output_file = output_dir+"/"+method+"_xdim_"+str(x_dim)
-    
-    # Call gpfa
-    result = None
-    result = gpfa_engine(seq_train=seq_train, seq_test=seq_test, fname=output_file,
-        x_dim=x_dim, bin_width=bin_width)
+        # Check if training data covariance is full rank
+        # TODO
+
+        # If doing cross-validation, don't use private noise variance floor
+        # TODO, set minVarFrac and pass to gpfa_engine
+
+        # Name of results file
+        output_file = output_dir+"/"+method+"_xdim_"+str(x_dim)
+        if num_folds > 1:
+            output_file += "_cv"+str(cvf)
+        
+        # Call gpfa
+        result = None
+        result = gpfa_engine(seq_train=seq_train, seq_test=seq_test, fname=output_file,
+            x_dim=x_dim, bin_width=bin_width)
 
     return result
